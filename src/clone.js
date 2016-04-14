bf779783f711bfacf2551b498024b4b998df22c9
Jan Henrik Overland <janhenrik.overland@gmail.com>
2016-04-14 22:34:38 +0200

    clone fix

--- src/clone.js
+++ src/clone.js
@@ -11,5 +11,5 @@
  * person1 !== person2 // Returns:  true
  */
 export default function clone(item) {
-    return JSON.parse(JSON.stringify(item));
+    return window.JSON.parse(window.JSON.stringify(item));
 }
